f652530e0611ddfcd259adc12bd7d2d09b284cb4
roymanter <cristobal.ramirez.puschel@gmail.com>
2014-11-04 01:43:12 -0300

    shit

--- twitter.lua
+++ twitter.lua
@@ -19,10 +19,12 @@ local function handler (evt)
   if evt.type == 'attribution' then
     if evt.name == 'text' then
       if evt.action == 'start' then
-        TEXT = evt.value -- Toma el valor de NCL
+        TEXT = evt.value -- Toma el valor de campo busqueda NCL
         url = url..TEXT
+        
         --evt.action = 'stop' -- Avisa que lo leyo cambiando el parametro action
-        --event.post(evt) -- Entre la clase evt modificado a NCL
+        --event.post(evt)     -- Entre la clase evt modificado a NCL
+        
       end
     end
   end
@@ -31,33 +33,11 @@ end
 event.register(handler)
 
 --background
-canvas:attrColor('navy')
-canvas:clear()
+--canvas:attrColor('navy')
+--canvas:clear()
 
-canvas:attrFont("Tiresias", 20, "normal") -- Se asignan sitintos tipos de letras con disitntos tamaños
-canvas:attrColor('blue') -- Se define el color de todo lo que se dibuje/escriba
+--canvas:attrFont("Tiresias", 20, "normal") -- Se asignan sitintos tipos de letras con disitntos tamaños
+--canvas:attrColor('blue') -- Se define el color de todo lo que se dibuje/escriba
 
 -- Implementa dentro de las funciones que tiene tcp.lua
- tcp.execute(
-        function ()
-			tcp.connect(HOST, 80)
-			tcp.send('GET '..url..' HTTP/1.1\r\n')
-			tcp.send('Host: '..HOST..'\r\n')		-- Es necesario 'Host: HTTP://'.. ???
-			tcp.send('\r\n')
-			
-			result = tcp.receive()
-			
-			if result then
-				_,_,answer = string.find(result, "<tweet>(.*)</tweet>")
-	    else
-		    result = 'error: '
-	    end
-	
-			-- La asignacion de estos parametros solo tiene valor dentro de la funcion
-			--canvas:drawText(30,200,'resultado: '..result)
-			--canvas:drawText(0,0,'Pregunta: '..answer)
-			canvas:flush()
-			
-			tcp.disconnect()
-		end
-)
+
